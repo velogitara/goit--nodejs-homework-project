3739462469c885178ccf08e20255dea830043cec
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-10-01 01:21:52 +0300

    add patch

--- models/contact.js
+++ models/contact.js
@@ -2,6 +2,8 @@ const { Schema, model } = require('mongoose');
 
 const Joi = require('Joi');
 
+// const emailRegex = /.+\@.+\..+/;
+
 const contactSchema = Schema(
   {
     name: {
@@ -10,7 +12,8 @@ const contactSchema = Schema(
     },
     email: {
       type: String,
-      required: true,
+      required: [true, 'Email address is required'],
+      // validate: [emailRegex, 'Please fill a valid email address'],
     },
     phone: {
       type: String,
