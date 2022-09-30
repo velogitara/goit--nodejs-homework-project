035d22b13b0d1969d84d0dc3c6ea0b03bc608060
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-09-30 21:00:58 +0300

    start 3rd

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
 const app = require('./app');
 
 const { PORT = 3000 } = process.env;
@@ -7,9 +13,6 @@ const { PORT = 3000 } = process.env;
 app.listen(PORT, () => {
   console.log('Server running. Use our API on port: 3000');
 });
-
-const DB_HOST =
-  'mongodb+srv://velogitara:axe25paKUyxgJyWL@cluster0.fi04lrd.mongodb.net/phoneBook?retryWrites=true&w=majority';
 
 mongoose
   .connect(DB_HOST)
